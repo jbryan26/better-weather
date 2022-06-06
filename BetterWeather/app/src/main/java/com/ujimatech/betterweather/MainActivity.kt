4f10f11b528a86a86afd0dbd85fad3fa1ad0677f
Jeremiah Bryant <Jbryan26@kent.com>
2022-06-06 00:17:43 -0400

    Added fragment and updated toolbar

--- BetterWeather/app/src/main/java/com/ujimatech/betterweather/MainActivity.kt
+++ BetterWeather/app/src/main/java/com/ujimatech/betterweather/MainActivity.kt
@@ -2,10 +2,18 @@ package com.ujimatech.betterweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ujimatech.betterweather.homescreen.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setIcon(R.drawable.ic_menu)
+        supportActionBar?.setTitle(R.string.toolbar_name)
+        if (savedInstanceState == null) {
+            val fragment = HomeFragment()
+            supportFragmentManager.beginTransaction().setReorderingAllowed(true).add(R.id.fragmentContainer, fragment, "homeFragment").commit()
+        }
     }
 }
